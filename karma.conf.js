04aeb81e0588004f803a2f03da61d90422c88541
jeeun-ko <pona0205@naver.com>
2025-12-17 16:32:52 +0900

    app

--- karma.conf.js
+++ karma.conf.js
@@ -1,50 +1,10 @@
-// // Karma configuration file, see link for more information
-// // https://karma-runner.github.io/1.0/config/configuration-file.html
-
-// module.exports = function (config) {
-//   config.set({
-//     basePath: '',
-//     frameworks: ['jasmine', '@angular-devkit/build-angular'],
-//     plugins: [
-//       require('karma-jasmine'),
-//       require('karma-chrome-launcher'),
-//       require('karma-jasmine-html-reporter'),
-//       require('karma-coverage-istanbul-reporter'),
-//       require('@angular-devkit/build-angular/plugins/karma')
-//     ],
-//     client: {
-//       clearContext: false // leave Jasmine Spec Runner output visible in browser
-//     },
-//     coverageIstanbulReporter: {
-//       dir: require('path').join(__dirname, './coverage/my-angular-project'),
-//       reports: ['html', 'lcovonly', 'text-summary'],
-//       fixWebpackSourcePaths: true
-//     },
-//     reporters: ['progress', 'kjhtml'],
-//     port: 9876,
-//     colors: true,
-//     logLevel: config.LOG_INFO,
-//     autoWatch: true,
-//     browsers: ['Chrome'],
-//     customLaunchers: {
-//       ChromeHeadlessCI: {
-//         base: 'ChromeHeadless',
-//         flags: ['--no-sandbox']
-//       }
-//     },
-//     singleRun: false,
-//     restartOnFileChange: true
-//   });
-// };
-
-// Karma configuration file
+// Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
-
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
@@ -52,40 +12,27 @@ module.exports = function (config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
-
     client: {
-      clearContext: false
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, './coverage/my-angular-project'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-
-    /** 🔴 CI에서 가장 중요한 부분 시작 */
-    autoWatch: false,
-
-    browsers: ['ChromeHeadlessCI'],
-
+    autoWatch: true,
+    browsers: ['Chrome'],
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
-        flags: [
-          '--no-sandbox',
-          '--disable-gpu',
-          '--disable-dev-shm-usage'
-        ]
+        flags: ['--no-sandbox']
       }
     },
-
-    singleRun: true,
-    restartOnFileChange: false
-    /** 🔴 CI에서 가장 중요한 부분 끝 */
+    singleRun: false,
+    restartOnFileChange: true
   });
 };
